888a7d751daace2f7a88e9a4d3504a5afa720da9
Zhenya <demichka@gmail.com>
2019-01-22 23:14:47 +0100

    boilerplate
    created project, installed node with packages, created index.js (server), added sass stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-zenCinema
+#zenCinema by grupp-don-3
+
+Begin with ```npm install```
+
+First starts mongoDB, next starts server (index.js)
---
+++ Server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// Require sass compiler
+const Sass = require('./sass');
+const config = require('./config.json');
+const fs = require('fs');
+const path = require('path');
+
+
+
+const flexjson = require('jsonflex')({
+    jsonDir: '/www/json', // directory on server to save json to
+    scriptUrl: '/jsonflex.js', // url to load clientside script from
+    saveUrl: '/json-save', // url used by jsonflex to save json
+    loadUrlPrefix: '/json/' // prefix to add to clientside load url
+});
+
+module.exports = class Server {
+
+    constructor() {
+        this.start();
+    }
+
+    async start() {
+        await this.connectToDb();
+        await this.startWebServer();
+    }
+
+    connectToDb() {
+        return new Promise((resolve, reject) => {
+            let dbName = 'db_zenCinema'
+            mongoose.connect(`mongodb://localhost/${dbName}`);
+            global.db = mongoose.connection;
+            db.on('error', () => reject('Could not connect to DB'));
+            db.once('open', () => resolve('Connected to DB'));
+        });
+    }
+
+    startWebServer() {
+
+        // Create a web server
+        const app = express();
+
+        // Add body-parser to our requests
+        app.use(bodyParser.json());
+
+        // Serve static files from www
+        app.use(express.static('www'));
+
+        // Automatically load all scripts at root level of js folder
+        // and load their corresponding template files
+        app.get('/autoload-js-and-templates', (req, res) => {
+            let files = fs.readdirSync(path.join(__dirname, '/www/js'));
+            files = files.filter(x => x.substr(-3) === '.js')
+            let html = files.map(x => `<script src="/js/${x}"></script>`).join('');
+            html += files.filter(x => fs.existsSync(path.join(
+                __dirname, '/www/templates', x.split('.js').join('.html')
+            ))).map(x => `<script src="/template-to-js/${
+      x.split('.js').join('.html')}"></script>`).join('');
+            res.send(`document.write('${html}')`);
+        });
+
+        // Convert a template to a js render method
+        app.get('/template-to-js/:template', (req, res) => {
+            let html = fs.readFileSync(path.join(
+                __dirname, '/www/templates', req.params.template));
+            html = req.params.template.split('.html')[0] +
+                '.prototype.render = function(){ return `\n' + html + '\n`};'
+            res.send(html);
+        });
+
+        // start the sass compiler
+        for (let conf of config.sass) {
+            new Sass(conf);
+        }
+
+        app.use(flexjson);
+
+        // Serve the index page everywhere so that the
+        // frontend router can decide what to do
+        app.use((req, res, next) => {
+            if (req.url === '/jsonflex.js' || req.url == '/json-save') {
+                next();
+                return;
+            }
+            res.sendFile(path.join(__dirname, '/www/index.html'));
+        });
+
+        // Start the web server
+        app.listen(3005, () => console.log('Go to the cinema on port 3005'));
+
+    }
+
+}
