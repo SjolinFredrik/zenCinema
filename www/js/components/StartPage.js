0c9c6fe5e78fc80376f11a5dbe060041a38b90dd
Zhenya <demichka@gmail.com>
2019-01-31 22:45:35 +0100

    fixed bug

--- www/js/components/StartPage.js
+++ www/js/components/StartPage.js
@@ -3,20 +3,19 @@ class StartPage extends Component {
   constructor(){
     super();
     this.addRoute('/', 'Start'); 
-    this.getFilmsFromShowings();
     this.today = new Date().toString().slice(0,10);
+    this.getFilmsFromShowings();
   }
 
   async showListForSeveralDaysFromToday(days) {
     let diffTime = days * 24 * 60 * 60 * 1000;
-    // let dateTo = new Date(new Date().getTime() + diffTime).toString();
     let lastDay = new Date().getTime() + diffTime;
     this.lastDay = new Date(lastDay).toString().slice(0,10);
     let actualShowings = await Showing.find(`.find({date: {$lte: ${lastDay}} }).populate('saloon').populate('film').exec()`);
     return actualShowings;
   }
 
-  async getFilmsFromShowings() {
+  getFilmsFromShowings() {
     this.showListForSeveralDaysFromToday(2).then(data => {
       this.actualShowings = data;
       // for (let i = 0; i < this.actualShowings.length; i++) {
@@ -27,9 +26,10 @@ class StartPage extends Component {
 
       this.actualFilms = [];
       let actualFilm = '';
-
+      console.log( this.actualShowings);
       for (let i = 0; i < this.actualShowings.length; i++) {
         let film = this.actualShowings[i].film;
+        console.log(film);
         actualFilm = new Film(film);
         this.actualFilms.push(actualFilm);
       }
