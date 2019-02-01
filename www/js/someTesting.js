ab38cad105ccd6e9fe265a4dd99c7e294b5c31d9
joel <joel.edling1@gmail.com>
2019-02-01 09:54:41 +0100

    test populate

--- www/js/someTesting.js
+++ www/js/someTesting.js
@@ -1,16 +1,39 @@
-async function testLogin(){
-}
+// async function testLogin(){
+// }
    
-  async function testCheckLogin(){
-    console.log(await Login.find());
-  }
+//   async function testCheckLogin(){
+//     console.log(await Login.find());
+//   }
    
-  async function testLogout(){
-    let loginObj = new Login();
-    console.log(await loginObj.delete());
-  }
+//   async function testLogout(){
+//     let loginObj = new Login();
+//     console.log(await loginObj.delete());
+//   }
 
-  // testLogin();
-  // testCheckLogin();
-  // testLogout();
+//   // testLogin();
+//   // testCheckLogin();
+//   // testLogout();
+async function showingsPopulatedFilms() {
+  let showings = await Showing.find();
+  
+  // for (let i = 0; i < showings.length; i++) {
+  //   let showing = showings[i];
+  //   let showingObj = new Showing(showing);
+  //   console.log(showingObj, 'I am showing');
+  //   console.log(showingObj.film);
+  // }
+  let filmShowings = [];
+  for (let i = 0; i < showings.length; i++) {
+    let showing = showings[i];
+    if (showing.film === '5c4af4448c106c1bac7b3df4') {
+      let showingObj = new Showing(showing);
+      filmShowings.push(showingObj);
+    }
+    else {
+      continue;
+    }
+    console.log(filmShowings);
+  }
+}
+showingsPopulatedFilms();
   
