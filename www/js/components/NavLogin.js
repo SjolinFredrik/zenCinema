852345e4556ade5fd4a3af0c92735c3c7d718532
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-02-05 11:16:36 +0100

    Added method for getting input values from nav login

--- www/js/components/NavLogin.js
+++ www/js/components/NavLogin.js
@@ -1,1 +1,15 @@
-class NavLogin extends Component {}
+class NavLogin extends Component {
+
+  constructor() {
+    super();
+    this.addEvents({
+      'click .login-btn': 'login'
+    });
+  }
+
+  login(e) {
+    e.preventDefault();
+    let email = this.baseEl.find('.email-login-input').val();
+    let password = this.baseEl.find('.password-login-input').val();
+  }
+}
