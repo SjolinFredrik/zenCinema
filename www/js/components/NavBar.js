11d9908806251af7ca3831d409fe6da64f510585
Jesper Asplund <jesper.asplund95@gmail.com>
2019-02-06 10:39:21 +0100

    hide booking and register navbar

--- www/js/components/NavBar.js
+++ www/js/components/NavBar.js
@@ -3,9 +3,9 @@ class NavBar extends Component {
   constructor() {
     super();
     this.navItems = [
-      new NavItem('Start', '/'),
-      new NavItem('Boka', '/booking'),
-      new NavItem('Registrera Användare', '/register')
+      new NavItem('Start', '/')
+      //new NavItem('Boka', '/booking'),
+      //new NavItem('Registrera Användare', '/register')
     ];
     this.navLogins = new NavLogin();
   }
