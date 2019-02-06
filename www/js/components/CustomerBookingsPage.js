67c90352d2aeaffc3576c0937ce866d6da51e92a
joel <joel.edling1@gmail.com>
2019-02-06 13:16:12 +0100

    booked films is now visible

--- www/js/components/CustomerBookingsPage.js
+++ www/js/components/CustomerBookingsPage.js
@@ -3,10 +3,12 @@ class CustomerBookingsPage extends Component {
   constructor() {
     super();
     this.addRoute('/mina-bokningar', 'Mina bokningar');
-    this.appendBookings();
+    this.films = [];
+    this.findBookings();
+    
   }
 
-  async appendBookings() {
+  async findBookings() {
     let user = await Login.find();
     if (!user.loggedIn) {
       console.log('Inte inloggad')
@@ -15,9 +17,27 @@ class CustomerBookingsPage extends Component {
       console.log('Inloggad')
       let userId = user.user._id;
 
-      let test = await User.find(`.findOne({_id: '${userId}'}).populate('bookings').exec()`);
-      console.log(test);
+      let customer = await User.find(`.findOne({_id: '${userId}'}).populate('bookings').exec()`);
+      let customerBookings = customer.bookings;
+
+      for (let booking of customerBookings) {
+        let showId = booking.show;
+        let showing = await Showing.find(`.findOne({_id: '${showId}'}).populate('film').exec()`);
+
+        let filmTitle = showing.film.title;
+        this.films.push(filmTitle);
+        console.log(this.films);
+      }
+      this.appendBookings();
     }
     
   }
+  appendBookings() {
+    for(let film of this.films) {
+      this.baseEl.find('.film-title').append(`<div> ${film}</div>`);
+    }
+
+  }
+
+
 }
