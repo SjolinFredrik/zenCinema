8d5c521bf48fd614dd0df7fd73e0f2ee36bce954
Jesper Asplund <jesper.asplund95@gmail.com>
2019-02-20 16:23:52 +0100

    StartPage is now finished +changed url route for saloonPage

--- www/js/components/SaloonTechInfoPage.js
+++ www/js/components/SaloonTechInfoPage.js
@@ -2,7 +2,7 @@ class SaloonTechInfoPage extends Component {
 
   constructor(){
     super();
-    this.addRoute('/om-oss/techinfo', 'Teknisk information');
+    this.addRoute('/om-oss/salonger', 'Salonger');
   }
 
 }
--- www/js/components/StartPage.js
+++ www/js/components/StartPage.js
@@ -2,7 +2,7 @@ class StartPage extends Component {
 
   constructor() {
     super();
-    this.addRoute('/', 'Start');
+    this.addRoute('/', '');
     this.calendarShowing = new CalendarShowing();
     this.filmCarousel = new FilmCarousel();
     this.zenCoin = new ZenCoin();
