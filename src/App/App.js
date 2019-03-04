8abc5f538b59bad88611a144184ee1ceb83b12ef
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-03-04 14:37:47 +0100

    Added content to the StartPage
    React component the whole grp did this!!!

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Here we want all of our fucking templates Jiimie Beeanman bitch
+import StartPage from '../StartPage/StartPage';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-        <h1 className="welcome">Welcome to ZenCinema!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={StartPage} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/StartPage/StartPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+
+export default class StartPage extends React.Component {
+  render() {
+    return (
+      <Container className="fluid">
+        <Row>
+          <Col xs="12" className="px-0">
+            <div className="startpage-carousel">
+              Filmkarusell
+              <div className="carousel-gradient" />
+            </div>
+          </Col>
+        </Row>
+        <Container className="main-container-fade text-dark">
+          <Row className="py-5">
+            <Col xs="12" md="6">
+              <Container className="fluid">
+                <Row>
+                  <Col xs="12" className="mb-5 rounded border border-secondary px-0">
+                    Här kommer ZenCoin
+                    <div className="bg-light border-top border-dark p-3">
+                      <h3>ZenCoins</h3>
+                      <p>Snart lanserar vi vår senaste nyhet, ZenCoins! Dessa kommer att intjänas genom de bokningar ni gör hos oss.</p>
+                    </div>
+                  </Col>
+                  <Col xs="12" className="mb-5 rounded border border-secondary px-0">
+                    <a href="/om-oss/salonger">
+                      <img className="img-fluid rounded-top" src="/images/cinema.jpg" alt="Bild på salong" />
+                    </a>
+                    <div className="bg-light border-top border-dark p-3">
+                      <h3>Våra salonger</h3>
+                      <p>Spana in lite information om våra utomordentligt mysiga salonger.</p>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+            <Col xs="12" md="6">
+              <Container className="fluid">
+                <Row>
+                  <Col xs="12" className="mb-5 rounded border cinema-link border-secondary px-0">
+                    <a href="/om-oss/kiosken">
+                      <img className="img-fluid rounded-top" src="/images/kiosk/jumbo.jpg" alt="Bild på godis" />
+                    </a>
+                    <div className="bg-light border-top border-dark p-3">
+                      <h3>Kiosken</h3>
+                      <p>Ta del av våra fantastiska erbjudanden!</p>
+                    </div>
+                  </Col>
+                  <Col xs="12" className="border px-0 border-secondary rounded mb-5">
+                    här kommer zonkydarioum
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    )
+  }
+}
