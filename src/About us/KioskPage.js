cdf042648b9d26a48ea1bb7f38508fff0eaa58ee
joel <joel.edling1@gmail.com>
2019-03-05 10:44:35 +0100

    fade fixer and height adjustments

--- src/About us/KioskPage.js
+++ src/About us/KioskPage.js
@@ -10,40 +10,34 @@ export default class KioskPage extends React.Component {
 
   render() {
     return (
-      <Container className="kiosk-content">
-        <div className="container-fluid">
-          <Row>
-            <div className="popcorn-img mx-auto px-0">
-              <img src="/images/kiosk/popcorn1.jpg" alt="big-popcorn" className="img-fluid" />
-              <div className="gradient-cover"></div>
-            </div>
-          </Row>
+      <Container fluid className="kiosk-content px-0">
+        <div className="popcorn-img mx-auto px-0">
+          <div className="gradient-cover"></div>
         </div>
-        <Container>
+        <div className="lower-fade px-0" />
+        <Container className="main-container-fade fade-fixer">
           <Row>
             <Col xs="12">
               <h1 className="kiosk-headline">Kiosken</h1>
             </Col>
           </Row>
-        </Container>
-        <Row>
-          <Col xs="12">
-            <div>
-              <p className="mb-2">
-                Här nedan hittar ni allt smarrigt vi har i kiosken.
-                </p>
-            </div>
-            <div>
-              <img src="/images/kiosk/candy-bear2.jpg" alt="Candy-bear-img" className="candy-bear-img" />
-            </div>
-            <p className="mt-2">
-              Våran absoluta storsäljare GodisBjörnen Björne säljer vi till 50% alltså ENDAST 2,45kr/kg! Mmmmm..
-                </p>
-          </Col>
-        </Row>
-        <Container>
           <Row>
-            <Col xs="12" md="6" className="px-0 mt-3">
+            <Col xs="12">
+              <div>
+                <p className="mb-2">
+                  Här nedan hittar ni allt smarrigt vi har i kiosken.
+                </p>
+              </div>
+              <div>
+                <img src="/images/kiosk/candy-bear2.jpg" alt="Candy-bear-img" className="candy-bear-img" />
+              </div>
+              <p className="mt-2">
+                Våran absoluta storsäljare GodisBjörnen Björne säljer vi till 50% alltså ENDAST 2,45kr/kg! Mmmmm..
+                </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="12" md="6" className="mt-3">
               <h2 className="drink-header">Avnjut prisbelönta drycker</h2>
               <div>
                 <p>
@@ -54,24 +48,22 @@ export default class KioskPage extends React.Component {
                   Erbjudandet gäller KÖP 3 och betala bara för EN dryck.
                       <br />
                   Så vad väntar du på!!
-                    </p>
+                </p>
               </div>
             </Col>
-            <Col xs="12" md="6" className="px-0 mt-3">
+            <Col xs="12" md="6" className="mt-3">
               <img src="/images/kiosk/soft-drinks2.jpg" alt="soft-drinks" className="img-fluid soft-drink-img" />
             </Col>
           </Row>
-        </Container>
-        <Container>
           <Row>
-            <Col xs="12" className="px-0 mt-4">
+            <Col xs="12" className="mt-4">
               <h2 className="jumbo-header">Megakampanj på JUMBO-boxen</h2>
               <img src="/images/kiosk/jumbo.jpg" alt="pop-drinks-and-awesome-price" className="img-fluid jumbo-img" />
               <p className="mt-2 mb-3">
                 I samarbete med läskdistributörer kan vi stolt presentera
                 dryck med nästan inget socker alls. Kidsen kommer till 100% sitta still. Vi leverar som vanligt
-                      pangpris där ett komplett paket landar på <strong>-ENDAST</strong> 29,99KR
-                    </p>
+                pangpris där ett komplett paket landar på <strong>-ENDAST</strong> 29,99KR
+              </p>
             </Col>
           </Row>
         </Container>
