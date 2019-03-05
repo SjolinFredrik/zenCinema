702891871c3dd933cc31a83c85b0567d3ecb47c6
Zhenya <demichka@gmail.com>
2019-03-06 00:16:38 +0100

    styled dropdown, added login-form for bookingPage

--- src/User/LoginForm.js
+++ src/User/LoginForm.js
@@ -9,7 +9,8 @@ import {
   Input,
   DropdownItem,
   UncontrolledDropdown,
-  DropdownToggle
+  DropdownToggle,
+  Col
 } from 'reactstrap';
 import REST from '../REST';
 import Login from '../Login';
@@ -60,20 +61,23 @@ export default class LoginForm extends React.Component {
     console.log(this.loggedIn, 'this.state.loggedIn');
 
     if (this.parent === 'NavBar') {
-      //  const isLoggedIn = this.state.loggedIn;
-      // console.log(isLoggedIn, 'isLogged in');
       if(this.loggedIn) {
-         result= <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="LoginToggle" data-toggle="dropdown"
-        aria-haspopup="true" aria-expanded="false">
-        Hej, ${this.loggedInUser.firstName}!
-        </button>
+         result= <div className="login-form"><ButtonGroup><UncontrolledDropdown>
+           <DropdownToggle  tag="button" type="button" className="btn btn-outline-secondary" caret>
+           Hej, ${this.loggedInUser.firstName}!
+           </DropdownToggle>
+           <DropdownMenu right className="dropdown-menu-lg-right login-menu">
+            <DropdownItem href="/mina-bokningar">Mina bokningar</DropdownItem>
+            <DropdownItem className="logout-btn mb-0">Logga ut</DropdownItem>
+           </DropdownMenu>
+         </UncontrolledDropdown></ButtonGroup></div>
       }
       else {
-        result = <UncontrolledDropdown >
+        result = <div className="login-form"><ButtonGroup><UncontrolledDropdown >
           <DropdownToggle tag="button" type="button" className="btn btn-outline-secondary" caret>
           Logga in
         </DropdownToggle>
-        <DropdownMenu className="dropdown-menu-lg-right login-menu">
+        <DropdownMenu right className="dropdown-menu-lg-right login-menu">
           <Form>
             <FormGroup >
                 <Label for="exampleDropdownFormEmail1">Epost</Label>
@@ -83,22 +87,39 @@ export default class LoginForm extends React.Component {
               <Label for="exampleDropdownFormPassword1">Lösenord</Label>
               <Input type="password" className="form-control password-login-input" id="exampleDropdownFormPassword1" placeholder="Password" />
             </FormGroup>
-            <Button className="btn btn-primary login-btn mt-2">Logga in</Button>
+            <Button color="primary" className="btn btn-primary login-btn mt-2">Logga in</Button>
           </Form>
           <DropdownItem divider />
                   
           <DropdownItem className="dropdown-item" href="/register">Registrera ny användare</DropdownItem>
         </DropdownMenu>
-        </UncontrolledDropdown>
+        </UncontrolledDropdown></ButtonGroup></div>
       }
     }
-      console.log(result);
+
+    else {
+      result = <div className="login-form d-flex justify-content-sm-center align-items-sm-center">
+           <Col sm="4">
+               <Form className="welcome">
+                  <h2>Logga in eller skapa nytt konto</h2>
+                  <FormGroup>
+                    <Label htmlFor="emailf">Epost</Label>
+                     <Input type="email" className="form-control email-login-input" id="emailf" placeholder="email@example.com" />
+                  </FormGroup>
+                   <FormGroup >
+                     <Label htmlFor="pwdf">Lösenord</Label>
+                     <Input type="password" className="form-control password-login-input" id="pwdf" placeholder="Password"/>
+                   </FormGroup>
+                   <Button className="btn-primary login-btn mt-2">Logga in</Button>
+                 </Form>
+                <Button class="btn-primary new-account-btn mt-2">Skapa konto</Button>
+          </Col>
+       </div>
+    }
 
     return (
-      <div className="login-form">
-        <ButtonGroup>
+      <div>
           {result}
-        </ButtonGroup>
       </div>
     )
   }
