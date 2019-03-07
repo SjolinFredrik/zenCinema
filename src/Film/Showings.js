3fa473d165405327d1504cc28493582d9d908316
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-03-07 11:59:00 +0100

    Create showing components and sort by time when the date the is the same

--- src/Film/Showings.js
+++ src/Film/Showings.js
@@ -1,39 +1,49 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Button
-} from 'reactstrap';
+import REST from '../REST';
+import ShowingComp from './Showing';
+
+class Showing extends REST {}
 
 export default class Showings extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.populateFilms();
+    this.showings = [];
+    this.state = {
+      showings: []
+    };
+  }
+
+  async populateFilms() {
+    let allShowings = await Showing.find(`.find().populate('film').populate('saloon')`);
+    const filmShowings = allShowings.filter(showing => showing.film._id === this.props.data._id);
+    let date = '';
+    let time = '';
+    for (let showing of filmShowings) {
+      console.log(date);
+      console.log(time);
+      if (showing.date === date) {
+        let index = this.showings.length - 1;
+        if (showing.time < time) {
+          this.showings.splice(index, 0, <ShowingComp key={showing._id} data={showing} />);
+          time = showing.time;
+        }
+        else {
+          this.showings.push(<ShowingComp key={showing._id} data={showing} />);
+          time = showing.time;
+        }
+      }
+      else {
+        this.showings.push(<ShowingComp key={showing._id} data={showing} />);
+        date = showing.date;
+        time = showing.time;
+      }
+    }
+    this.setState({ showings: this.showings });
+  }
+
   render() {
-    return (
-      <Col xs="12" md="10" className="offset-md-1 px-0 text-left">
-        <Container fluid>
-          <Row className="px-0">
-            <Col xs="4" md="3" className="pl-2 pr-0">
-              <strong>Salong:</strong><br />
-              Salongsnamn
-            </Col>
-            <Col xs="3" className="film-title d-none d-md-block">
-              <strong>Film:</strong><br />
-              Filmtitel
-            </Col>
-            <Col xs="3" className="pl-2 px-md-0">
-              <strong>Datum:</strong><br />
-              Datum
-            </Col>
-            <Col xs="2" md="1" className="px-0">
-              <strong>Tid:</strong><br />
-              Tid
-            </Col>
-            <Col xs="3" md="2" className="pr-2 my-auto">
-              <Button title="Boka FILMTITEL" className="btn btn-secondary float-right book-film">Boka</Button>
-            </Col>
-          </Row>
-        </Container>
-      </Col>
-    );
+    return this.state.showings;
   }
 }
