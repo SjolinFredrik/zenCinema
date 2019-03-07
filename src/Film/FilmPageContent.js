e6a03071216a8ec4185ec12ae882a30e3505949e
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-03-07 12:12:14 +0100

    Removed comment

--- src/Film/FilmPageContent.js
+++ src/Film/FilmPageContent.js
@@ -17,7 +17,6 @@ export default class FilmPageContent extends React.Component {
         </Row>
         <Container className="main-container-fade">
           <Row className="film-page-shows">
-          {/* Förstår col nedanför vad attributet id är? */}
             <Col xs="12" className="pt-3 pb-5 px-0 px-md-5" id="film-shows">
               <h2 className="mb-5 text-center">Visningar</h2>
               <Showings data={filmProps} />
