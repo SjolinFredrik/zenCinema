b5d021065206f0f294d0a97dfc9184b92a32e6c4
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-03-13 14:57:06 +0100

    Added some unfinnished html to CustomerBooking

--- src/UserBooking/CustomerBooking.js
+++ src/UserBooking/CustomerBooking.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default class CustomerBooking extends React.Component {
-  
+  constructor(props) {
+    super(props)
+    
+  }
+
+  render() {
+    return (
+      <div class="usersBooking">
+        <dl>
+          <dt>{this.props.film}</dt>
+          <dd>{this.props.date}</dd>
+          <dd>{this.props.time}</dd>
+          <dd>{this.props.bookingNr}</dd>
+        </dl>
+      </div>
+    )
+  }
 }
