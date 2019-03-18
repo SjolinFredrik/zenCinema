071f4821d313d8719843b23e8f836d244f35c872
Zhenya <demichka@gmail.com>
2019-03-18 14:25:32 +0100

    BookingSystem and BookingSummary are bound

--- src/Booking/BookingSummary.js
+++ src/Booking/BookingSummary.js
@@ -3,20 +3,23 @@ import { } from 'reactstrap'
 
 export default class BookingSummary extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
 
   }
+
+
   render() {
     return (
       <div className="booking-summary clearfix">
-        <img className="img-fluid d-block float-left" src="/images/movies/${this.film.images[0]}" alt="${this.film.title}" />
+        <img className="img-fluid d-block float-left" src={'/images/movies/' + this.props.film.images[0]} alt={this.props.film.title}  />
         <dl className="text-left">
-          <dt>{this.film.title}</dt>
-          <dd>{Math.floor(this.film.length / 60)} tim {this.film.length % 60} min</dd>
-          <dd>Salong: {this.saloonName}</dd>
-          <dd>Tid: {this.showingDate ? this.showingDate : ''} | {this.time}</dd>
-          <dd>Valda platser: {Store.chosenSeats && Store.chosenSeats.length > 0 ? Store.chosenSeats.sort().join(', ') : 'Välj plats(er)'}</dd>
-          <dd>Att Betala: {Store.reservedTickets ? Store.reservedTickets + ' SEK' : 'Välj biljetter'}</dd>
+          <dt>{this.props.film.title}</dt>
+          <dd>{Math.floor(this.props.film.length / 60)} tim {this.props.film.length % 60} min</dd>
+          <dd>Salong: {this.props.saloonName.name}</dd>
+          <dd>Tid: {this.props.showingDate !==null ? this.props.showingDate : ''} | {this.props.showingTime}</dd>
+          <dd>Valda platser: {this.props.selectedSeats && this.props.selectedSeats !== undefined ? this.props.selectedSeats.sort().join(', ') : 'Välj plats(er)'}</dd>
+          <dd>Att Betala: {this.props.ticketsCost ? this.props.ticketsCost + ' SEK' : 'Välj biljetter'}</dd>
         </dl>
       </div>
     )
