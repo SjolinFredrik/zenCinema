25a5a58520787caeed67e91f8f26d5768fa5ae0b
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-03-20 12:05:11 +0100

    Moved toggle method into this component and commented out checkIfAdmin

--- src/Admin/AdminPage.js
+++ src/Admin/AdminPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   Container,
   Row,
-  Col
+  Col,
+  Button
 } from 'reactstrap';
 import ManageShowing from './ManageShowing'
 import Showings from './Showings'
@@ -13,11 +14,15 @@ import MissingPage from '../MissingPage/MissingPage';
 export default class AdminPage extends React.Component {
   constructor() {
     super()
-    this.state = { admin: '' }
-    this.checkIfAdmin()
-
+    this.state = { 
+      admin: '',
+      modal: true,
+      modalComponent: ''
+    }
+    //this.checkIfAdmin()
+    this.toggle = this.toggle.bind(this);
   }
-  async checkIfAdmin() {
+  /* async checkIfAdmin() {
     let user = await Login.find()
     if (user.loggedIn && user.user.admin) {
       this.setState({
@@ -39,9 +44,33 @@ export default class AdminPage extends React.Component {
           <MissingPage />
       })
     }
+  } */
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal,
+      modalComponent: this.state.modal ? <ManageShowing isOpen={this.state.modal} toggle={this.toggle} /> : ''
+    }))
   }
 
   render() {
-    return this.state.admin
+    //return this.state.admin
+    return (
+      <Container className="main-container-fade">
+      <Row>
+        <Col xs="12">
+        <Button color="danger" onClick={this.toggle}>Klicka</Button>
+        </Col>
+      </Row>
+            <Row>
+              <Col xs="12">
+                {this.state.modalComponent}
+              </Col>
+              <Col xs="12" className="p-0">
+                <Showings />
+              </Col>
+            </Row>
+          </Container>
+    )
   }
 }
