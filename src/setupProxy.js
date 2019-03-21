015dd95f8643e626594ba23e514cfab24012082a
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-03-21 16:36:26 +0100

    Fuck off IO-beanman

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,6 @@
 const proxy = require('http-proxy-middleware');
  
 module.exports = function(app) {
-  // added proxy for socket.io so the damned thing can work
-  app.use(proxy('/socket.io/', { target: 'http://localhost:3001' 
-  }));
   app.use(proxy('/json/', { target: 'http://localhost:3001/' 
   }));
 };
