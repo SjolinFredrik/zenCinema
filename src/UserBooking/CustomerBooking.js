b8c84e02701a5b268c1f0b2d52d01e9dd7cdecad
Jesper Asplund <jesper.asplund95@gmail.com>
2019-03-21 16:07:11 +0100

    bookinghistory now also shows what seats a booking has

--- src/UserBooking/CustomerBooking.js
+++ src/UserBooking/CustomerBooking.js
@@ -8,6 +8,7 @@ export default class CustomerBooking extends React.Component {
           <td>{this.props.film}</td>
           <td>{new Date(this.props.date).toLocaleDateString('sv-SE', {weekday: 'short', month: 'long', day: 'numeric'})}</td>
           <td>{this.props.time}</td>
+          <td>[{this.props.seats.join('] [')}]</td>
           <td>{this.props.bookingNr}</td>
         </tr>
     )
