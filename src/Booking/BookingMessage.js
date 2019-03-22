b85e5eba98434d79523e3a5b23bac0591f0ae0ef
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-03-22 11:03:16 +0100

    More beauty fixes to our beloved homepage

--- src/Booking/BookingMessage.js
+++ src/Booking/BookingMessage.js
@@ -6,7 +6,6 @@ class Showing extends REST { }
 export default class BookingMessage extends React.Component {
   constructor(props) {
     super(props)
-    // this.removeMe = this.removeMe.bind(this)
     this.state = {
       type: this.props.type,
       data: this.props.data,
@@ -15,7 +14,7 @@ export default class BookingMessage extends React.Component {
       time: '',
       date: '',
       saloon: ''
-    };
+    }
   }
 
   componentDidMount() {
@@ -31,10 +30,7 @@ export default class BookingMessage extends React.Component {
           date: new Date(data[0].date).toLocaleString('sv-SE', { month: 'long', day: 'numeric', year: 'numeric' }),
           saloon: data[0].saloon.name
         })
-
-      });
-      // console.log(this.state.data);
-      
+      })      
     }
 
     if(this.state.type === 'chooseSeats') {
@@ -63,13 +59,6 @@ export default class BookingMessage extends React.Component {
     return showData;
   }
 
-  // removeMe() {
-  //   let greatLogin = global.STORE.navBar.navLogins;
-  //   greatLogin.checkLogin();
-  // }
-
-
-
   render() {
     return (
       <div className="message-wrap">
@@ -97,30 +86,4 @@ export default class BookingMessage extends React.Component {
       </div>
     )
   }
-
-
-
-  // //     <div class="message-wrap">
-  // //     <div class="col-sm-7 mx-auto message">
-  // //         <h3>{this.heading}</h3>
-  // //         <p>{this.text}</p>
-  // //        {this.type === 'newBooking' ? `
-  // //        <p>
-  // //             Bokningsnummer: ${this.props.bookingNumber}<br/>
-  // //             Salong: ${this.saloon}<br/>
-  // //             Film: ${this.filmTitle}<br/>
-  // //             Platser: ${this.props.seats.sort().join(', ')}<br/>
-  // //             Tid: ${this.time}<br/>
-  // //             Datum: ${this.date}<br/>
-  // //             Att betala: ${this.props.totalCost}
-  // //           </p>
-  // //           <button type="button" data-dismiss="modal" aria-label="Close" class="btn btn-secondary close-and-goto" >Stäng</button>
-  // //        ` :
-  // //        this.type !== 'newBooking' ? `
-  // //            <button type="button" class="btn btn-secondary close-message" >Stäng</button>
-  // //        ` :
-  // //        ''}
-  // //       </div>
-  // // </div>
-  //   )
 }
