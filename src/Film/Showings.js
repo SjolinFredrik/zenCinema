d75df1f931de34b4c20bcc33d26c7509dda26ae4
Zhenya <demichka@gmail.com>
2019-03-22 11:56:00 +0100

    fixed empty container

--- src/Film/Showings.js
+++ src/Film/Showings.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import REST from '../REST';
 import ShowingComp from './Showing';
+import { Container,  Row, Col } from 'reactstrap';
+
 
 class Showing extends REST { }
 
@@ -29,18 +31,23 @@ export default class Showings extends React.Component {
   render() {
     let content = '';
     if(this.props.data !== undefined )  {
-      content = <div>
-      <h2 className="mb-5 text-center">Visningar</h2>
-      {this.state.showings.map(showing => {
-        return <ShowingComp key={showing._id} data={showing} auth={this.props.auth} showBookingPage={this.props.showBookingPage} />
-      })}
-      </div>
+      content =
+      <Row className="film-page-shows">
+            <Col xs="12" className="pt-3 pb-5 px-0 px-md-5" id="film-shows">
+            <h2 className="mb-5 text-center">Visningar</h2>
+              {this.state.showings.map(showing => {
+              return <ShowingComp key={showing._id} data={showing} auth={this.props.auth} showBookingPage={this.props.showBookingPage} />
+            })}
+            </Col>
+          </Row>
+      
+      
     }
     
     return (
-      <div>
-        {content}
-      </div>
+      <Container className="main-container-fade">
+          {content}
+        </Container>
     )
   }
 }
